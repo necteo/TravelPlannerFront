809e41faee8eb8d897ce87750f12eb7cfea5d4cf
PMO <rhkdah9090@gmail.com>
2022-11-14 00:56:50 +0900

    2022_11-14 12:57 OT_Test

--- TravelPlanner-master/App.js
+++ TravelPlanner-master/App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { TextInput, View } from "react-native";
+import { View } from "react-native";
 
 import { Body } from "./components/Body";
 import { styles } from "./Styles";
-import { Test } from "./components/OT_Test";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Test></Test>
+      <StatusBar style="dark" />
+      <Body />
     </View>
   );
 }
--- TravelPlanner-master/components/OT_Test.js
+++ TravelPlanner-master/components/OT_Test.js
@@ -1,14 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
 import { View, TextInput } from "react-native";
-
-import { Body } from "./components/Body";
-import { styles } from "./Styles";
 
 const Test = ({}) => {
   return (
     <View>
-      <TextInput></TextInput>
+      <View>123</View>
     </View>
   );
 };
