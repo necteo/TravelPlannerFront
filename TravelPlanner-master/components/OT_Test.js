8d74d4bfaeee036797e720924d3d41c49c6cbd72
PMO <rhkdah9090@gmail.com>
2022-11-14 00:14:39 +0900

    테스트

---
+++ TravelPlanner-master/components/OT_Test.js
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, TextInput } from "react-native";
+
+import { Body } from "./components/Body";
+import { styles } from "./Styles";
+
+const Test = ({}) => {
+  return (
+    <View>
+      <TextInput></TextInput>
+    </View>
+  );
+};
+
+export default Test;
