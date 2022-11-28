1656ea49fa3d0928e91ddb6424741a65aac7c433
PMO <rhkdah9090@gmail.com>
2022-11-28 10:41:37 +0900

    //

--- TravelPlanner-master/components/OT_Test.js
+++ TravelPlanner-master/components/OT_Test.js
@@ -3,26 +3,19 @@ import { View, TextInput, Text, ScrollView } from "react-native";
 import { styles } from "../Styles";
 import React, { useState, KeyboardEvent } from "react";
 
+import { PostTools } from "./PostTool";
+
 export const Test = ({}) => {
+  const postTool = new PostTools("http://192.168.0.6:3000/");
   const [strings, setStrings] = useState("");
 
   const updateTest = (text) => {
-    fetch("http://192.168.0.5:3000/OtTest/change", {
-      method: "POST",
-      headers: {
-        //Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: text,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        //console.log("Success :" + data.name);
-        setStrings(data.name);
-      })
-      .catch((error) => console.log("Error : " + error));
+    let data = postTool.postWithData(
+      "OtTest/change",
+      JSON.stringify({ name: text })
+    );
+    //console.log(data);
+    //setStrings(data);
   };
 
   return (
