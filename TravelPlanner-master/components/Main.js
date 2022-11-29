2f5f84587b86d18a6dc2896ce04bbacf180b86d0
PMO <rhkdah9090@gmail.com>
2022-11-30 00:06:26 +0900

    중간점검

--- TravelPlanner-master/components/Main.js
+++ TravelPlanner-master/components/Main.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Text, View, Image, ScrollView, TouchableOpacity } from "react-native";
+import { PostTools } from "./PostTool";
 
 const Member = ({ id }) => {
   return (
--- TravelPlanner-master/components/PostTool.js
+++ TravelPlanner-master/components/PostTool.js
@@ -4,42 +4,15 @@ export class PostTools {
   constructor(ip) {
     this.serverIp = ip;
   }
-  post(url) {
-    fetch(this.serverIp + url, {
+  async post(url) {
+    const data = await fetch(this.serverIp + url, {
       method: "POST",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        return JSON.stringify(data);
-      })
-      .catch((error) => console.log("Error : " + error));
-  }
+    }).then((response) => response.json());
 
-  aa(url, reqData) {
-    new Promise((resolve, reject) => {
-      let res;
-      fetch(this.serverIp + url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: reqData,
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          res = JSON.stringify(data);
-          res = "ss";
-          console.log("then");
-        })
-        .catch((error) => console.log("Error : " + error));
-      resolve(res);
-    }).then(() => {
-      console.log("return");
-    });
+    return JSON.stringify(data);
   }
 
   async postWithData(url, reqData) {
-    let res;
     const data = await fetch(this.serverIp + url, {
       method: "POST",
       headers: {
@@ -47,8 +20,7 @@ export class PostTools {
       },
       body: reqData,
     }).then((response) => response.json());
-    res = JSON.stringify(data);
 
-    return res;
+    return JSON.stringify(data);
   }
 }
