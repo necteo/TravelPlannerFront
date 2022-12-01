60091d74add88508c9087269f2d4c73adce7a5e0
necteo <cauresn06@gmail.com>
2022-12-01 19:44:32 +0900

    master

--- TravelPlanner-master/components/Main.js
+++ TravelPlanner-master/components/Main.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Text, View, Image, ScrollView, TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { PostTools } from "./PostTool";
 
 const Member = ({ id }) => {
